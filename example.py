88c6fafd4bbd14fa1324f6d5d75f64f674df9a91
VegaDeg <frmisok@gmail.com>
2022-06-03 15:19:58 +0900

    Major upgrade
    Deleted unnecessary code and added some utility, now it can be used with least but stable functions.

--- example.py
+++ example.py
@@ -1,6 +1,12 @@
-import timing as ti
-from timing.event import displayEvent
+import src.timepipe.event as ti
+import time
 
 event1 = ti.event("cjb")
 event1.check()
-displayEvent()
+time.sleep(1)
+event1.check()
+time.sleep(2)
+event1.check()
+
+event1.eventHistory()
+print(event1.framerate())
--- src/timepipe/event.py
+++ src/timepipe/event.py
@@ -1,18 +1,33 @@
 import time
 
-objectList = []
-
 class event():
+    def __init__(self):
+        self.eventName = "Event"
+        self.timeList = []
+        self.counter = 0
+
     def __init__(self, eventName):
         self.eventName = eventName
         self.timeList = []
-        objectList.append(self)
+        self.counter = 0
 
     def check(self):
         self.timeList.append(time.time())
+        self.counter = self.counter + 1
+        
+    def framerate(self):
+        if self.counter <= 1:
+            return 0
+        else:
+            return (self.timeList[-1] - self.timeList[0]) / (self.counter - 1)
 
-def displayEvent():
-    print(len(objectList))
-    for i in range(0, len(objectList)-1):
-        print(i)
-        print(objectList[i].eventName + " to " + objectList[i+1].eventName + " took " + str(objectList[i+1].timeList[0]-objectList[i].timeList[0]) + " seconds")
+    def eventHistory(self):
+        if self.counter == 0:
+            print("No check history")
+        elif self.counter == 1:
+            print("Only check at: " + self.timeList[0])
+        else:
+            print("first check at: " + str(self.timeList[0]))
+            for i in range(1, self.counter):
+                line = str(i+1) + "-th check at " + str(self.timeList[i] - self.timeList[i-1]) + " seconds later"
+                print(line)
