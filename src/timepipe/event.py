f28436251441fe15dbba5f8f2fb13dad4f17f5c4
VegaDeg <frmisok@gmail.com>
2022-06-03 12:02:05 +0900

    Built as a python package

--- src/timepipe/event.py
+++ src/timepipe/event.py
@@ -16,16 +16,3 @@ def displayEvent():
     for i in range(0, len(objectList)-1):
         print(i)
         print(objectList[i].eventName + " to " + objectList[i+1].eventName + " took " + str(objectList[i+1].timeList[0]-objectList[i].timeList[0]) + " seconds")
-
-
-event1 = event("event 1")
-event2 = event("event 2")
-event3 = event("event 3")
-
-event1.check()
-time.sleep(1)
-event2.check()
-time.sleep(1)
-event3.check()
-
-displayEvent()
--- timing/__init__.py
+++
@@ -1,1 +0,0 @@
-from timing.event import event
